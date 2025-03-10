0263597b3ddea20f0721ac31e7e0ed22c7661e94
danna <dannayisel22@gmail.com>
2025-03-10 10:24:26 -0500

    jerarquia

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -7,12 +7,30 @@ Acorde a las tablas de Verdad
 '''
 # Ejemplo 1: Operador not:
 y = not True
-print("El resultadode de operarar con not es", y)
+print("El resultado de operar con not es", y)
 
 # Ejemplo 2: Operador and:
 y = False and True
-print("El resultadode de operarar con and es", y)
+print("El resultado de operar con and es", y)
 
 # Ejemplo 3: Operador or:
 y = False or False
-print("El resultadode de operarar con or es", y)
+print("El resultado de operar con or es", y)
+
+'''
+Jerarquia de presendecia de operadores
+(loicos inclusive)
+1.            ()
+2.            **
+3.            /, //, %   
+4.            +, - 
+5.            >, <, >=, <=, !=, ==
+6.            not
+7.            and
+8.            or
+9.            =
+'''
+
+# Ejemplo 4: Jerarquia de presendecia de operadores
+y = False and not True or False
+print("El resultado de operar con jerarquia de operaciones es", y)
